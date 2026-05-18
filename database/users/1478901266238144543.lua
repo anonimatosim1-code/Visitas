1c0b39b597d65e654660ddb2fadb6e48e13eef37
anonimatosim1-code <anonimatosim1@gmail.com>
2026-05-18 19:48:28 -0300

    nova playlist Oxe

--- database/users/1478901266238144543.lua
+++ database/users/1478901266238144543.lua
@@ -2,7 +2,7 @@
 -- CONFIGURAÇÕES DO BOT (EDITAR AQUI)
 -- ==========================================================
 
-_G.PLAYLISTS = {"Todas", "Aleatórias", "NSFW", "Memes", "Players"}
+_G.PLAYLISTS = {"Todas", "Aleatórias", "NSFW", "Memes", "Players", "Oxe"}
 
 -- ⬇⬇⬇ SLOTS (BOT EDITA AQUI) ⬇⬇⬇
 _G.ARTS = {
